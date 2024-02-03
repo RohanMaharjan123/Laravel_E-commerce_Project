7053d68b0f70ce05959056322607cd730c73149d
RohanMaharjan123 <nahor.uzamaki@gmail.com>
2024-02-04 04:14:22 +0545

    Setting up database, database migration and creating Authcontroller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //Show login page
+    public function showLogin()
+    {
+        return view('pages.login');
+    }
+
+    //show register page
+    public function showRegister()
+    {
+        return view('pages.register');
+    }
+
+    //Register User 
+
+    // Login User 
+    
+
+    //Password Reset
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[PagesController::class,'home'])->name('home');
 Route::get('cart',[PagesController::class,'cart'])->name('cart');
+
+//Auth
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
